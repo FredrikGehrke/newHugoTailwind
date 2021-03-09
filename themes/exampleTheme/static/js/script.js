d566818d04257d06262e7bfcd354172ea4315f91
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2021-03-09 09:41:08 +0100

    new navbar

--- themes/exampleTheme/static/js/script.js
+++ themes/exampleTheme/static/js/script.js
@@ -1,29 +1,51 @@
-const navToggler = document.querySelector('#navToggler')
-const menu = document.querySelector('#menu')
+// let openVariable
 
-if(window.innerWidth > 1023) {
-    menu.classList.remove('hidden')
-} else {
-    menu.classList.add('hidden')
-}
+// if(window.innerWidth > 1023) {
+//     openVariable = true
+// } else {
+//     openVariable = false
+// }
+// console.log(openVariable)
 
-// Mobil: 
-navToggler.addEventListener('click', ()=>{
-    if(menu.classList.contains('hidden') && window.innerWidth < 1023){
-        menu.classList.remove('hidden')
-    } else{
-        menu.classList.add('hidden')
-    }
-});
 
-// Stor skärm:
-window.addEventListener('resize', () =>{
-    if(window.innerWidth > 1023){
-        menu.classList.remove('hidden')
-    } else {
-        menu.classList.add('hidden')
-    }
-})
+// function openMenu() {
+//     return {
+        
+//         show: openVariable,
+//         open() { this.show = true },
+//         close() { this.show = false },
+//         isOpen() { return this.show === true },
+//     }
+// }
+
+
+// const navToggler = document.querySelector('#navToggler')
+// const menu = document.querySelector('#menu')
+
+// if(window.innerWidth > 1023) {
+//     menu.classList.remove('hidden')
+// } else {
+//     menu.classList.add('hidden')
+// }
+
+// // Mobil: 
+// navToggler.addEventListener('click', ()=>{
+//     if(menu.classList.contains('hidden') && window.innerWidth < 1023){
+//         menu.classList.remove('hidden')
+//     } else{
+//         menu.classList.add('hidden')
+//     }
+// });
+
+// // Stor skärm:
+// window.addEventListener('resize', () =>{
+//     if(window.innerWidth > 1023){
+//         menu.classList.remove('hidden')
+//     } else {
+//         menu.classList.add('hidden')
+//     }
+// })
+
 
 
 
